d524808dcb456cc9483709ad420ced63d1303864
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-08-30 14:38:57 -0500

    database connected

---
+++ db/db.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+let connectionString 
+
+if(process.env.NODE_ENV === "production"){
+	connectionString = process.env.DB_URL
+} else {
+	connectionString = 'mongodb://localhost/nasadata'
+}
+
+mongoose.connect(connectionString,{
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useFindAndModidy: false 
+})
+
+mongoose.connection.on('connected',() => {
+	console.log('Mongoose is connected');
+})
+
+mongoose.connection.on('err', (err) => {
+	console.log('mongoose error: ',err );
+})
+
+mongoose.connection.on('disconnected', () => {
+	console.log('mongoose is disconnected');
+})
--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 require ('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const session = require('express-session')
 const app = express()
 const PORT = process.env.PORT
+
+require('./db/db')
+
+
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	saveUninitialized: false
+}))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const corsOptions = {
+	origin: process.env.ORIGIN,
+	credentials: true,
+	optionSuccessStatus: 200
+}
 
 
 app.listen(process.env.PORT, () => {

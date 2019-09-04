0d02ae0c473be08c8463d51db63c3bbb934261b9
YUZHU CHAI <yuzhupoemhead@gmail.com>
2019-09-03 21:15:02 -0500

    find commet by id and find all comments of one post

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -30,6 +30,63 @@ router.post('/:postId', async (req,res)=>{
 // 5d6ed6806e94435a37dba203 this is the test post id 
 
 
+router.get('/', async (req,res)=>{
+	try{
+		const findAllComments = await Comment.find()
+		res.status(200).json({
+			message: 'success',
+			code: 200,
+			data: findAllComments
+		})
+
+	}catch(err){
+		res.status(500).json({
+			message: 'internal server error',
+			error: err,
+			success: false 
+		})
+	}
+})
+
+
+// get all the commet from one Post
+router.get('/post/:id', async (req,res)=>{
+	try{
+		const findCommnets = await Comment.find({post: req.params.id})
+		res.status(200).json({
+			message: 'success',
+			code: 200,
+			data: findCommnets
+		})
+		
+	}catch(err){
+		res.status(500).json({
+			message: 'internal server error',
+			error: err,
+			success: false 
+		})
+	}
+}) 
+
+
+// get one commet  
+router.get('/:id', async (req,res)=>{
+	try{
+		const findOneComment = await Comment.findById(req.params.id)
+		res.status(200).json({
+			message: 'success',
+			code: 200,
+			data: findOneComment
+		})
+		
+	}catch(err){
+		res.status(500).json({
+			message: 'internal server error',
+			error: err,
+			success: false 
+		})
+	}
+})
 
 module.exports = router 
 
